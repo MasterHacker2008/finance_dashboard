176bb6d73ca24082d72ce6f1c1f1f528ae53726f
arjun <arjunkunder321@gmail.com>
2025-05-04 17:19:19 +0100

    Adding the project report

--- pages/Project Report.py
+++ pages/Project Report.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Risk-Adjusted Commodities Portfolio", layout="wid
 st.title("📊 Risk-Adjusted Commodities Portfolio")
 st.markdown("**By Matthew Reynolds and Arjun Kunder**")
 
-st.markdown("🔗 [View Full Report PDF](https://github.com/MasterHacker2008/finance_dashboard.git)")
+st.markdown("🔗 [View Full Report PDF](https://github.com/MasterHacker2008/finance_dashboard/blob/main/ALT2_Commodities_Analysis_Report.pdf)")
 
 st.markdown(
     """
